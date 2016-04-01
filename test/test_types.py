1c2d8955c7817f68d864321a16293b1b8b77c0a7
Franz Steinmetz <franz.steinmetz@dlr.de>
2016-04-01 16:25:04 +0200

    New test: nested types

--- test/test_types.py
+++ test/test_types.py
@@ -1,5 +1,11 @@
 from testing_utils import convert_with_assertion
 
+
 def test_types():
     for _type in [int, float, str, type, complex, list, dict, set, tuple, type(None)]:
         convert_with_assertion(_type)
+
+
+def test_nested_type():
+    convert_with_assertion([int, type(None)])
+    convert_with_assertion([{'a': float, 'b': (complex, set([set, dict]))}])
