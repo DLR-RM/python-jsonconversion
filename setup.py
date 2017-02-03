9e1d57fb492f10d33647f168df55fd548db0d470
Franz Steinmetz <franz.steinmetz@dlr.de>
2017-02-03 09:59:55 +0100

    Use pytest-runner for running tests
    - remove test class
    - add dependency to pytest-runner
    - add setup.cfg to configure tests to use pytest

--- setup.py
+++ setup.py
@@ -1,31 +1,4 @@
-import sys
 from setuptools import setup, find_packages
-from setuptools.command.test import test as TestCommand
-
-
-class PyTest(TestCommand):
-    """Run py.test with JSON conversion tests
-
-    Copied from https://pytest.org/latest/goodpractises.html#integrating-with-setuptools-python-setup-py-test
-    """
-    # This allows the user to add custom parameters to py.test, e.g.
-    # python setup.py test -a "-v"
-    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
-
-    def initialize_options(self):
-        TestCommand.initialize_options(self)
-        self.pytest_args = ['-xs']
-
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        # import here, cause outside the eggs aren't loaded
-        import pytest
-        error_number = pytest.main(self.pytest_args)
-        sys.exit(error_number)
 
 
 setup(
@@ -43,8 +16,7 @@ setup(
     packages=find_packages('src'),  # include all packages under src
     package_dir={'': 'src'},   # tell distutils packages are under src
 
+    setup_requires=['pytest-runner'],
     install_requires=[],
-    tests_require=['pytest', 'numpy'],
-
-    cmdclass={'test': PyTest},
+    tests_require=['pytest', 'numpy']
 )
