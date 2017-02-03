6af5c21a166b7d3144227156adc3bfb88a84fcc9
Franz Steinmetz <franz.steinmetz@dlr.de>
2017-02-03 14:47:37 +0100

    Add tests for JSONObjects nested in builtins
    Also adjust outputs of log

--- test/test_jsonobjects.py
+++ test/test_jsonobjects.py
@@ -17,13 +17,11 @@ class JO(JSONObject):
         return {'a': self.a, 'b': self.b, 'c': self.c}
 
     def __eq__(self, other):
-        print "types", type(self.a), type(other.a)
-        print self.a, self.b, self.c
-        print other.a, other.b, other.c
         return self.a == other.a and self.b == other.b and self.c == other.c
 
     def __str__(self):
-        return "{0}: a = {1}, b = {2}, c = {3}".format(self.__class__.__name__, self.a, self.b, self.c)
+        return "{0}: a = {1} {4}, b = {2} {5}, c = {3} {6}".format(self.__class__.__name__, self.a, self.b, self.c,
+                                                                   type(self.a), type(self.b), type(self.c))
 
 
 def test_simple_objects():
@@ -38,6 +36,16 @@ def test_simple_objects():
     convert_with_assertion(j3)
     convert_with_assertion(j4)
     convert_with_assertion(j5)
+
+
+def test_nested_objects():
+    l1 = [JO(1, 2, 3), JO('1.1', "3", "0")]
+    t1 = tuple(l1[:])
+    d1 = {0: l1[:], 1: tuple(t1)}
+
+    convert_with_assertion(l1)
+    convert_with_assertion(t1)
+    convert_with_assertion(d1)
 
 
 def test_complex_objects():
