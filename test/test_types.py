1162b89514fc7afffd2614a6e350f40487286d16
Franz Steinmetz <franz.steinmetz@dlr.de>
2017-03-31 10:56:39 +0200

    Add copyright header
    - copyright notice
    - link to license
    - contributors

--- test/test_types.py
+++ test/test_types.py
@@ -1,4 +1,12 @@
-# Copyright
+# Copyright (C) 2016-2017 DLR
+#
+# All rights reserved. This program and the accompanying materials are made
+# available under the terms of the 2-Clause BSD License ("Simplified BSD
+# License") which accompanies this distribution, and is available at
+# https://opensource.org/licenses/BSD-2-Clause
+#
+# Contributors:
+# Franz Steinmetz <franz.steinmetz@dlr.de>
 
 from testing_utils import convert_with_assertion
 
