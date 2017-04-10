7de473106ae81291cf6efd34f2ee383236d5df24
Franz Steinmetz <franz.steinmetz@dlr.de>
2017-04-10 15:06:55 +0200

    Set correct version number

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open(path.join(here, 'README.rst')) as f:
 
 setup(
     name='jsonconversion',
-    version='0.2.2',
+    version='0.2.3',
     url='https://github.com/DLR-RM/python-jsonconversion',
     download_url='https://github.com/DLR-RM/python-jsonconversion/archive/0.2.2.tar.gz',
     license='BSD',
