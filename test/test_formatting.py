af99c70041c58028942dc422915694c5fed5abe5
Franz Steinmetz <franz.steinmetz@dlr.de>
2019-06-18 13:10:27 +0200

    test(formatting): ensure consistent separators

--- test/test_formatting.py
+++ test/test_formatting.py
@@ -3,7 +3,7 @@ import json
 from jsonconversion.encoder import JSONObjectEncoder
 
 def test_dict():
-    dump = json.dumps({"b": 2.3, 'a': 1}, cls=JSONObjectEncoder, indent=4, sort_keys=True)
+    dump = json.dumps({"b": 2.3, 'a': 1}, cls=JSONObjectEncoder, indent=4, sort_keys=True, separators=(',', ': '))
     target_dump = \
 """{
     "a": 1,
